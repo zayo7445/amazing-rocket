5de29f7ad600c4c55189c7755dbffd7123d9463c
Simon Fikse <sifi8953@student.uu.se>
2026-09-25 11:32:32 +0200

    constant angle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import numpy as np
 from scipy.integrate import solve_ivp
+from scipy.optimize import fsolve
+from scipy.spatial.distance import cdist
 import matplotlib.pyplot as plt
 from collections import namedtuple
 
@@ -11,7 +13,7 @@ KM = 700.0
 C = 0.05
 G = np.array([0, -9.82])
 
-TIME = 20
+TIME = 10
 
 INIT_LAUNCH_HEIGHT = 20
 
@@ -21,39 +23,63 @@ GOAL_POS = np.array([80, 60])
 
 X_AXIS = np.array([1, 0])
 
-def theta(t, v, pos):
-    if pos[1] < INIT_LAUNCH_HEIGHT:
+H = 0.01
+
+
+def angle_between(p1, p2):
+    vec = p1 - p2
+    unit = vec / np.linalg.norm(vec)
+    return np.arctan2(unit[1], unit[0])
+
+
+def theta_toward(t, p, v):
+    if p[1] < INIT_LAUNCH_HEIGHT:
         return -np.pi/2
     else:
-        thrust = pos - GOAL_POS
-        unit = thrust / np.linalg.norm(thrust) 
-        return np.arctan2(unit[1], unit[0])
+        return angle_between(p, GOAL_POS)
+
+
+def theta_constant(angle):
+    def theta(t, p, v):
+        if p[1] < INIT_LAUNCH_HEIGHT:
+            return -np.pi/2
+        else:
+            return angle
+    return theta
+
 
 def m(t):
     return ROCKET_MASS + max(FUEL_MASS - BURN_RATE * t, 0)
 
+
 def dm(t):
     return -BURN_RATE if t * BURN_RATE <= FUEL_MASS else 0
 
-def u(t, v, pos):
-    alpha = theta(t, v, pos)
-    return KM * np.array([np.cos(alpha), np.sin(alpha)])
+
+def u(theta):
+    return KM * np.array([np.cos(theta), np.sin(theta)])
+
 
 def F(t, v):
     return m(t) * G - C * np.linalg.norm(v) * v
 
-def a(t, v, pos):
-    return (F(t, v) + dm(t) * u(t, v, pos)) / m(t)
 
-def fun(t, y):
-    posx, posy, vx, vy = y
-    v = np.array([vx, vy])
-    pos = np.array([posx, posy])
-    ax, ay = a(t, v, pos)
-    print(vx, vy, ax, ay)
-    return np.array([vx, vy, ax, ay])
+def a(t, v, u):
+    return (F(t, v) + dm(t) * u) / m(t)
+
+
+def fun(theta, t, y):
+    pos, vel = np.split(y, 2)
+    acc = a(t, vel, u(theta(t, pos, vel)))
+    return np.concatenate([vel, acc])
+
+
+def system(theta):
+    return lambda t, y: fun(theta, t, y)
+
 
 OdeResult = namedtuple("OdeResult", ["t", "y"])
+
 
 def rk4(f, tspan, y0, h):
     steps = round((tspan[1] - tspan[0]) / h)
@@ -71,10 +97,22 @@ def rk4(f, tspan, y0, h):
     return OdeResult(t, np.transpose(y))
 
 
+# sol = solve_ivp(system(theta_toward), [0, TIME], np.concatenate([START_POS, START_VEL]))
+# sol = rk4(system(theta_toward), [0, TIME], np.concatenate([START_POS, START_VEL]), H)
+
+def find_angle(angle):
+    angle, = angle
+    sol = rk4(system(theta_constant(angle)), [0, TIME], np.concatenate([START_POS, START_VEL]), H)
+    dist = cdist(np.transpose(sol.y[:2]), np.array([GOAL_POS]))
+    return dist.min()
 
 
-# sol = solve_ivp(fun, [0, TIME], [*START_POS, *START_VEL])
-sol = rk4(fun, [0, TIME], [*START_POS, *START_VEL], 0.01)
+angle = angle_between(START_POS, GOAL_POS)
+angle, = fsolve(find_angle, angle)
+
+print(angle)
+
+sol = rk4(system(theta_constant(angle)), [0, TIME], np.concatenate([START_POS, START_VEL]), H)
 
 plt.plot(sol.y[0], sol.y[1], "--")
 plt.plot(START_POS[0], START_POS[1], "or")
